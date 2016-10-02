f16dfe10db7715f50ff8dbb259701d7a3fa3465b
SriniV <sm@gmail.com>
2016-10-02 23:02:43 +1100

    prevent errors from minifiction

--- assignment-1/scripts/app.js
+++ assignment-1/scripts/app.js
@@ -13,15 +13,16 @@ function LunchCheckController($scope,
   $scope.lunchMenu = '';
 
   $scope.checkLunchMenu = function() {
-    var lunchMenuLength = $scope.lunchMenu.split(',');
+    var lunchMenu = $scope.lunchMenu.split(',');
 
-    if ( lunchMenuLength.length > 3 ) {
-      $scope.style = 'border-color:red';
-      alert('Too much!');
-    }
-    else {
+    if ( lunchMenu[0] == '' ) {
+      alert("Please enter data first");
+    } else if ( lunchMenu.length <= 3 ) {
       $scope.style = 'border-color:green';
       alert('Enjoy!');
+    } else {
+      $scope.style = 'border-color:red';
+      alert('Too much!');
     }
   };
 
