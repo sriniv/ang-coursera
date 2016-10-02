3040ebc7fd60de6fb7278346941d4f2d56da3dd6
SriniV <sm@gmail.com>
2016-10-02 23:47:30 +1100

    modified to ng-style

--- assignment-1/scripts/app.js
+++ assignment-1/scripts/app.js
@@ -14,22 +14,22 @@ function LunchCheckController($scope,
 
   $scope.checkLunchMenu = function() {
     var lunchMenu = $scope.lunchMenu.split(',');
-    var message = '';
+    $scope.message = '';
 
     if ( lunchMenu[0] == '' ) {
-      message = 'Please enter data first';
+      $scope.message = 'Please enter data first';
     } else if ( lunchMenu.length <= 3 ) {
       $scope.style = {
           "border-color" : "green"
       };
-      message = 'Enjoy!';
+      $scope.message = 'Enjoy!';
     } else {
       $scope.style = {
           "border-color" : "red"
       };
-      message = 'Too much!';
+      $scope.message = 'Too much!';
     }
-    alert(message);
+
   };
 
 };
